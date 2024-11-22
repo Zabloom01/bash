16a63c4c58fb51ddbc2021e64bb1f2365b098b94
Zach <Zabloom01@gmail.com>
2024-11-21 22:47:10 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -9,7 +9,7 @@ echo "Welcome! Let's build the source code!"
 # By default head returns 10 lines, but -n helps us specify the number of lines we want
 firstline=$(head -n 1 source/changelog.md)
 # Split firstline into an array and assign it to splitfirstline. Note, <<< is a here string
-read -a splitfirstline <<< "$firstline"
+read -a splitfirstline <<< "$firstline" # Read input from stream - read can either read from a stream or take an input from a user
 
 # Access value of index 1 and assign version to variable 'version'
 version=${splitfirstline[1]}
@@ -18,8 +18,8 @@ echo "You are building version $version"
 # Give the user a chance to exit the script if they need to make a version change
 # If user enters "1" (for yes) continue, else "0" (for no) to exit
 echo 'Do you want to continue? (enter "1" for yes, "0" for no)'
-read versioncontinue
-if [ "$versioncontinue" -eq 1 ]; then 
+read versioncontinue # record user input
+if [ "$versioncontinue" -eq 1 ]; then # Note this if statement is functionally equivalent to the later if statement.  Either style works
   echo "OK"
 else
   echo "Please come back when you are ready"
@@ -31,16 +31,17 @@ mkdir -p build
 
 # Copy every file from source to build, skipping source/secretinfo.md
 echo "Copying files from source to build..."
-for filename in source/*; do
-  if [ "$filename" == "source/secretinfo.md" ]; then
-    echo "Skipping file: $filename"
+for filename in source/*; do # Asterisk (*) means it will look at all files in the source directory
+  if [ "$filename" == "source/secretinfo.md" ] # Note this if statement is functionally equivalent to the prior if statement, either style works
+  then
+    echo "Skipping file: $filename" # skip file, does not copy to build directory
   else
     echo "Copying file: $filename"
     cp "$filename" build/ || { echo "Failed to copy $filename"; exit 1; }
   fi
 done
 
-# List files in the build directory
+# List files in the build directory to confirm file transfer was successful
 echo "Files in the build directory:"
-#
+# list the build files.
 ls build/
